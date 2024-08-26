4c946a2e343deaca701de54dc2d0a1653240f8fe
nguyentrongban <nguyentrongban01052003@gmail.com>
2024-08-26 11:25:11 +0700

    update mail
    cập nhật gửi mail

---
+++ LocalReview/src/main/java/com/localreview/DTO/UserRegistrationDto.java
@@ -0,0 +1,12 @@
+package com.localreview.DTO;
+
+import lombok.Data;
+
+@Data
+public class UserRegistrationDto {
+	  private String email;
+	    private String name;
+	    private String storeName;
+	    private String address;
+	    private String phoneNumber;
+}
